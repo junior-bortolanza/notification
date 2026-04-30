00f958d97a15ffe50d98fc034b62b2a0dcdf8357
Junior Bortolanza <gbortolanzajr@gmail.com>
2026-04-30 10:26:03 -0300

    initial commit

---
+++ notificacao/src/main/groovy/com/bortolanza/notificacao/NotificacaoApplication.groovy
@@ -0,0 +1,13 @@
+package com.bortolanza.notificacao
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NotificacaoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(NotificacaoApplication, args)
+	}
+
+}
